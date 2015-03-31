fec3deb56695babd479c405912198dc84c30d236
Arkosys <arxosys@gmail.com>
2015-03-31 22:54:27 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MusicWebApp::Application.config.secret_key_base = 'ea7c50dc5aa80e01074ca6aafcd42b1f474c6d6496b89f4adfb581537a05f9284225a2a2ab7e01cf89a0a322d8290de9bcef24e9cefc75033aa4d831a1c63435'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MusicWebApp::Application.config.session_store :cookie_store, key: '_MusicWebApp_session'
